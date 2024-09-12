cc8f6132f9f0121f52525d60a53a45ca757bc842
pabpeiser <pabpeiser@alu.edu.gva.es>
2024-09-12 11:50:40 +0200

    aaa

--- E2/src/Main.java
+++ E2/src/Main.java
@@ -1,12 +1,7 @@
 class Notification {
     void show(int num) {
-        if (num > 99) {
-            System.out.println("You have +99 new messages");
-        } else if (num == 1) {
-            System.out.println("You have 1 new message");
-        }else {
-            System.out.println("You have " + num + " new messages");
-        }
+        System.out.println("You have " + num + " new messages");
+
     }
 }
 class anonym extends Notification {}
@@ -14,7 +9,19 @@ class anonym extends Notification {}
 public class Main {
     public static void main(String[] args) {
 
-        anonym notification = new anonym();
+        Notification notification = new Notification() {
+            void show(int num) {
+                System.out.println("You have " + num + " new messages");
+
+                if (num > 99) {
+                    System.out.println("You have +99 new messages");
+                } else if (num == 1) {
+                    System.out.println("You have 1 new message");
+                } else {
+                    System.out.println("You have " + num + " new messages");
+                }
+            }
+        };
 
         notification.show(1);
         notification.show(10);
